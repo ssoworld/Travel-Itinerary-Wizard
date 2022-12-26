30dbd77e39c86fd6443365b1a626824313eed977
Scott Onson <onsons@ssoworld.com>
2022-12-26 15:11:02 -0600

    postbuild and switch to collector

--- executetest.ps1
+++ executetest.ps1
@@ -1,2 +1,3 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:ExcludeByAttribute="CompilerGenerated"
-reportgenerator -reports:"XUnit.Coverlet.MSBuild\coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:Html
+dotnet test  --collect:"XPlat Code Coverage" --settings coverlet.runsettings /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
+# dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:ExcludeByAttribute="CompilerGenerated"
+reportgenerator -reports:"XUnit.Coverlet.Collector\testResults\**\coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:Html
---
+++ postBuild/postbuild.bat
@@ -0,0 +1,2 @@
+xcopy /y $(ProjectDir)data\properties.jsonc $(ProjectDir)$(OutDir)
+xcopy /y $(ProjectDir)data\air_travel.jsonc $(ProjectDir)$(OutDir)
