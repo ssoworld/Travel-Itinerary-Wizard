b44b55e5e18b009489629a158f8174c63826731b
Scott Onson <onsons@ssoworld.com>
2024-01-14 16:43:37 -0600

    fix: expand testing and upgrade actions (#33)
    * setup UTs
    
    * got xunit working
    
    * rm redundant file
    
    * rm unwanted files
    
    * cov isolation and cleanup
    
    * ReportGenerator
    
    * fix indent
    
    * lower for debug
    
    * syntax fix
    
    * syntax fix
    
    * syntax fix
    
    * postbuild and switch to collector
    
    * postbuild and switch to collector
    
    * more new code
    
    * replace the old coverlet specific with this model
    
    * fix syntax
    
    * fix syntax
    
    * fix syntax
    
    * fix syntax
    
    * fix syntax
    
    * fix syntax
    
    * disable step
    
    * fix: expand testing
    
    * rm whitesource
    
    * update dependencies and test report
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * Revert "fix test and coverage"
    
    This reverts commit 7fe0a91a5b4a9f95d65037b820db1634eeb08c8f.
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * fix test and coverage
    
    * coverage by matrix
    
    * fix os on group 3
    
    * debug coverage report
    
    * need to rename the file
    
    * archive the HTML and codeql
    
    * root branch in ql and do we need node
    
    * repairing codeql
    
    * repairing codeql
    
    * debugging codeql
    
    * debugging PR comment

--- executetest.ps1
+++ executetest.ps1
@@ -1,3 +1,4 @@
-dotnet test  --collect:"XPlat Code Coverage" --settings coverlet.runsettings /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
+dotnet test   -c Debug --collect:"XPlat Code Coverage" --verbosity normal  --settings coverlet.runsettings /p:CollectCoverage=true --results-directory ./coverage
+cp coverage/**/coverage.cobertura.xml coverage.cobertura.xml
 # dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:ExcludeByAttribute="CompilerGenerated"
 reportgenerator -reports:"XUnit.Coverlet.Collector\testResults\**\coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:Html
